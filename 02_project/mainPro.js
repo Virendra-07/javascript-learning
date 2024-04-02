90d24f6426db5bdcb900f2fc0763e9d956cc6404
Virendra-07 <154392251+Virendra-07@users.noreply.github.com>
2024-04-02 22:04:30 +0530

    Update mainPro.js

--- 02_project/mainPro.js
+++ 02_project/mainPro.js
@@ -1,28 +1,4 @@
-// console.log("Virendra Kumare");
-// const buttons = document.querySelectorAll(".button");
-// const body = document.querySelector('body');
 
-// buttons.forEach(function(button){
-//     console.log(button);
-//     button.addEventListener('click', function(e){
-//         console.log(e);
-//         console.log(e.target);
-//         if(e.target.id === 'gray'){
-//             body.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === 'white'){
-//             body.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === 'blue'){
-//             body.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === 'yellow'){
-//             body.backgroundColor = e.target.id;
-//         }
-//     });
-// });
-
-// uper wala nhi likhna h
 
 // console.log("Virendra Kumar");
 const buttons = document.querySelectorAll('.button');
